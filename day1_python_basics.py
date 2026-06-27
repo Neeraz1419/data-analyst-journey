cc75aa0ba9770bb6932a23753b14c79f3e985e13
Neeraz1419 <neeraz1419@email.com>
2026-06-27 05:41:44 +0300

    Day 1: Python Basics - first commit

---
+++ day1_python_basics.py
@@ -0,0 +1,9 @@
+name = "Niraj"
+goal = "Data Analyst"
+days_completed = 15
+
+print("My name is", name)
+print("My goal is to become a", goal)
+print("Days completed:", days_completed)
+
+print("Let's go!")
